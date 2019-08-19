cffdd0f4fbf712cd6396695f84a2c746cb143016
Tan <tanweerkbaig@gmail.com>
2019-08-19 14:06:08 +0100

    Commit 12.4

--- Server/sequelize.js
+++ Server/sequelize.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize')
 const ProjectModel = require('./models/project')
 
-const sequelize = new Sequelize('portfolio', 'root', '', {
+const sequelize = new Sequelize('portfolio', 'tan', 'newPass123', {
   host: 'localhost',
   dialect: 'mysql',
   pool: {
